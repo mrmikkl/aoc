534b01161dbf9c57b2448ea38e880bd93cfbd874
Gödecke, Maik <maik.goedecke@god.de>
2022-12-10 21:48:55 +0100

    2022 - Day 2 - Prepare alternative solution

--- src/main/kotlin/de/dev/aoc/year2022/day2/Day2Main.kt
+++ src/main/kotlin/de/dev/aoc/year2022/day2/Day2Main.kt
@@ -1,7 +1,7 @@
 package de.dev.aoc.year2022.day2
 
 import de.dev.aoc.ResourceLoader
-import de.dev.aoc.year2022.day2.inheritance.StrategyGuideMove
+import de.dev.aoc.year2022.day2.inheritance.StrategyGuideInstruction
 
 
 fun main() {
@@ -13,22 +13,22 @@ fun main() {
 
 private fun solveDay2SimpleInputWithInheritance() {
     val strategyGuideStrings: List<String> = ResourceLoader.loadLines("/year2022/day2/simple_input.txt")
-    val strategyGuideMoves: List<StrategyGuideMove> = StrategyGuideMove.byOpponentAndMyMoves(strategyGuideStrings)
-    val myScore: Int = strategyGuideMoves.fold(0) { scoreSum, strategyGuideMove -> scoreSum + strategyGuideMove.determineMyScore() }
+    val strategyGuideInstructions: List<StrategyGuideInstruction> = StrategyGuideInstruction.byOpponentAndMyMoves(strategyGuideStrings)
+    val myScore: Int = strategyGuideInstructions.fold(0) { scoreSum, strategyGuideMove -> scoreSum + strategyGuideMove.determineMyScore() }
     println("Day 2 - Simple Input - Solution is: $myScore")
 }
 
 fun solveDay2Part1WithInheritance() {
     val strategyGuideStrings: List<String> = ResourceLoader.loadLines("/year2022/day2/input.txt")
-    val strategyGuideMoves: List<StrategyGuideMove> = StrategyGuideMove.byOpponentAndMyMoves(strategyGuideStrings)
-    val myScore: Int = strategyGuideMoves.fold(0) { scoreSum, strategyGuideMove -> scoreSum + strategyGuideMove.determineMyScore() }
+    val strategyGuideInstructions: List<StrategyGuideInstruction> = StrategyGuideInstruction.byOpponentAndMyMoves(strategyGuideStrings)
+    val myScore: Int = strategyGuideInstructions.fold(0) { scoreSum, strategyGuideMove -> scoreSum + strategyGuideMove.determineMyScore() }
     println("Day 2 - Part 1 - Input - Solution is: $myScore")
 }
 
 fun solveDay2Part2WithInheritance() {
     val strategyGuideStrings: List<String> = ResourceLoader.loadLines("/year2022/day2/input.txt")
-    val strategyGuideMoves: List<StrategyGuideMove> = StrategyGuideMove.byOpponentMoveAndWishedResult(strategyGuideStrings)
-    val myScore: Int = strategyGuideMoves.fold(0) { scoreSum, strategyGuideMove -> scoreSum + strategyGuideMove.determineMyScore() }
+    val strategyGuideInstructions: List<StrategyGuideInstruction> = StrategyGuideInstruction.byOpponentMoveAndWishedResult(strategyGuideStrings)
+    val myScore: Int = strategyGuideInstructions.fold(0) { scoreSum, strategyGuideMove -> scoreSum + strategyGuideMove.determineMyScore() }
     println("Day 2 - Part 2 - Input - Solution is: $myScore")
 }
 
--- src/main/kotlin/de/dev/aoc/year2022/day2/inheritance/StrategyGuideInstruction.kt
+++ src/main/kotlin/de/dev/aoc/year2022/day2/inheritance/StrategyGuideInstruction.kt
@@ -1,32 +1,32 @@
 package de.dev.aoc.year2022.day2.inheritance
 
-class StrategyGuideMove(private val opponentMove: Move, private val myMove: Move) {
+class StrategyGuideInstruction(private val opponentMove: Move, private val myMove: Move) {
     fun determineMyScore(): Int {
         return myMove.determineScore(opponentMove)
     }
 
     companion object {
-        fun byOpponentAndMyMoves(inputs: List<String>): List<StrategyGuideMove> {
+        fun byOpponentAndMyMoves(inputs: List<String>): List<StrategyGuideInstruction> {
             return inputs.map { byOpponentAndMyMoves(it) }
         }
 
-        private fun byOpponentAndMyMoves(input: String): StrategyGuideMove {
+        private fun byOpponentAndMyMoves(input: String): StrategyGuideInstruction {
             val splittedInput: List<String> = input.split(" ")
             val opponentMove: Move = MoveFactory.byEncryption(splittedInput[0])
             val myMove: Move = MoveFactory.byEncryption(splittedInput[1])
-            return StrategyGuideMove(opponentMove, myMove)
+            return StrategyGuideInstruction(opponentMove, myMove)
         }
 
-        fun byOpponentMoveAndWishedResult(inputs: List<String>): List<StrategyGuideMove> {
+        fun byOpponentMoveAndWishedResult(inputs: List<String>): List<StrategyGuideInstruction> {
             return inputs.map { byOpponentMoveAndWishedResult(it) }
         }
 
-        private fun byOpponentMoveAndWishedResult(input: String): StrategyGuideMove {
+        private fun byOpponentMoveAndWishedResult(input: String): StrategyGuideInstruction {
             val splittedInput: List<String> = input.split(" ")
             val opponentMove: Move = MoveFactory.byEncryption(splittedInput[0])
             val wishedResult: Result = Result.byEncryption(splittedInput[1])
             val myMove: Move = MoveFactory.byResult(wishedResult, opponentMove)
-            return StrategyGuideMove(opponentMove, myMove)
+            return StrategyGuideInstruction(opponentMove, myMove)
         }
     }
 
